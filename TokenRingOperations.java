c269828870c65b93ef8b95c5f7e777aa1f270b9a
cljvf9 <cljvf9@rc04xcs213.managed.mst.edu>
2012-03-12 16:52:02 -0500

    started the GUI

--- TokenRingOperations.java
+++ TokenRingOperations.java
@@ -1,13 +1,23 @@
 import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class TokenRingOperations {
-    public static void main(String[] args) {
-        new TokenRingOperations();
-    }
-
-    public TokenRingOperations() {
-
-        //create the network
+public class TokenRingOperations extends JApplet implements ActionListener {
+   // public static void main(String[] args) {
+   //     new TokenRingOperations();
+   // }
+   
+	private JButton startButton, stopButton, moveButton, requestButton;
+	private JTextField source, destination;
+	private JTextArea log;
+	private Container content;
+	private JPanel controlPanel, controlPanel1;
+	private JList schemeList;
+	private JTextArea [] mssArea;
+   
+    public void init() {
+		//create the network
         Network TRnet = new Network();
 
         //create some MSS
@@ -42,6 +52,56 @@ public class TokenRingOperations {
         MH mobile_9 = new MH("mobile_9", cell_c);
 
         TRnet.update();
+		
+		content = getContentPane();
+		
+		startButton = new JButton("Start the Simulation");
+		startButton.addActionListener(this);
+		
+		stopButton = new JButton("Stop the simulation");
+		stopButton.addActionListener(this);
+		
+		requestButton = new JButton("Request the Token");
+		requestButton.addActionListener(this);
+		
+		moveButton = new JButton("Move MH's");
+		moveButton.addActionListener(this);
+		
+		String[] selections = {"MH's Only","Inform Strategy","Replication Strategy"};
+		schemeList = new JList(selections);
+		source = new JTextField("Mobile ID", 10);
+		destination = new JTextField("destination", 10);
+		
+		JLabel label = new JLabel("to");
+		
+		controlPanel = new JPanel();
+		controlPanel.add(startButton);
+		controlPanel.add(stopButton);
+		controlPanel.add(moveButton);
+		controlPanel.add(requestButton);
+		controlPanel.add(source);
+		controlPanel.add(label);
+		controlPanel.add(destination);
+		controlPanel.add(schemeList);
+		
+		content.add(controlPanel, BorderLayout.NORTH);
+		mssArea = new JTextArea[MSSlist.size()];
+		controlPanel1 = new JPanel();
+		
+		for(int i= 0; i<TRnet.getAllMSSs().size(); i++){
+			MSS cell = (MSS)TRnet.getAllMSSs().get(i);
+			mssArea[i].setRows(10);
+			mssArea[i].setColumns(10);
+			mssArea[i].setText("");
+			mssArea[i].setVisible(true);
+			controlPanel1.add(new JScrollPane(mssArea[i]));
+			mssArea[i].append(cell.getID());
+			for (int j=0; j<cell.getLocalMHs().size(); j++) {
+				mssArea[i].append( ((MH)cell.getLocalMHs().get(j)).getID()  );
+			}
+		}
+        content.add(controlPanel1, BorderLayout.CENTER);
+		content.add(new JLabel("dummy"), BorderLayout.SOUTH);
 
         //run a few tests of MSS facilitated token passing
         mobile_1.request();   
@@ -54,4 +114,6 @@ public class TokenRingOperations {
 		//Inform algo = new Inform(TRnet);
 		Replication algo = new Replication(TRnet);
     }
+	
+	public void actionPerformed(ActionEvent e){}
 }
